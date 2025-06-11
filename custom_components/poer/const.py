0e5d90481070feb74534bdf95d1a0e50ee45c48e
lilingbo <li_lingbo@lancable.cn>
2025-06-11 06:53:45 +0000

    prod url

--- custom_components/poer/const.py
+++ custom_components/poer/const.py
@@ -7,8 +7,8 @@ from typing import Final
 
 DOMAIN: Final = "poer"
 ISMOCK: Final = False
-CNURL: Final = "http://10.11.0.150:6666"
-EUURL: Final = "http://10.11.0.150:6666"
+CNURL: Final = "https://open2.poersmart.com"  # "http://10.11.0.150:6666"
+EUURL: Final = "https://open.poersmart.com"  # "http://10.11.0.150:6666"
 
 
 _LOGGER = logging.getLogger(__name__)
